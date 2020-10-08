13fc5ab2972aeda2e6c56835b39aa3740fb44b64
Sanket Wadekar <67091512+sanketwadekar@users.noreply.github.com>
2020-10-08 23:05:06 +0530

    Packages updated to fix vulnerability issues

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-const dbPassword = 'mongodb+srv://sanket:sanket@cluster0-smpyj.mongodb.net/blog?retryWrites=true&w=majority'; //MongoDB URI String
+const dbPassword = 'MONGOURI'; //MongoDB URI String
 
 module.exports = {
     mongoURI: dbPassword
